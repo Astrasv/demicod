2c33c49ac714cdce0b080c08a86f0923a3ed3476
Astrasv <sudharsan.vanamali@gmail.com>
2026-05-09 09:11:46 +0530

    Feat: Wire up extension entry point with state manager and commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { StateManager } from './stateManager';
+import { registerCommands } from './commands';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+        vscode.window.showErrorMessage('Demicod requires an open workspace folder.');
+        return;
+    }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "demicod" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('demicod.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DemiCod!');
-	});
-
-	context.subscriptions.push(disposable);
+    const stateManager = new StateManager(workspaceFolders[0].uri.fsPath);
+    registerCommands(context, stateManager);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
